5f94683787ccbb79b3f311a90b4529fe9bf7e1f7
Eghosa Ordia <eghordia130@gmail.com>
2025-03-17 10:32:41 +0100

    first commit

---
+++ frontend/src/constants/media.tsx
@@ -0,0 +1,11 @@
+import logo from "../assets/qr code logo.png";
+import home from "../assets/home.png";
+import person from "../assets/person.png";
+import upload from "../assets/upload icon.png";
+
+export const icons = {
+  logo,
+  home,
+  person,
+  upload,
+};
