01cff86034cb9672770b272e7da8f9e284d7363d
Eghosa Ordia <eghordia130@gmail.com>
2025-03-17 10:43:35 +0100

    Refactor App component to include Navbar and remove unused code

---
+++ frontend/src/components/navbar.tsx
@@ -0,0 +1,16 @@
+import { icons } from "../constants/media";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="fixed w-full top-0">
+      <div className="w-3/4 flex justify-between items-center px-16 py-8 mx-auto">
+        <img src={icons.logo} alt="logo" className="w-10" />
+        <a href="#" className="font-poppins-bold">
+          Create Profile
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
