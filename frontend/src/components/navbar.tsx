6100f06ce9152d26221ee333dad890d8bc12cc3a
Eghosa Ordia <eghordia130@gmail.com>
2025-03-17 10:54:12 +0100

    Update color variable and adjust font styles in Navbar and Home components

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar: React.FC = () => {
     <nav className="fixed w-full top-0">
       <div className="w-3/5 flex justify-between items-center py-8 mx-auto">
         <img src={icons.logo} alt="logo" className="w-10" />
-        <a href="#" className="font-poppins-bold">
+        <a href="#" className="font-poppins-medium">
           Create Profile
         </a>
       </div>
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,7 @@
 const Home: React.FC = () => {
   return (
     <section id="hero">
-      <div className="w-3/5 mx-auto">
+      <div className="w-3/5 mx-auto text-purple-1">
         <h2 className="font-poppins-bold text-2xl">Features</h2>
       </div>
     </section>
