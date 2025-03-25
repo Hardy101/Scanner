6ca55ae0d3fb9df57489051f553bde2ff50da70b
Eghosa Ordia <eghordia130@gmail.com>
2025-03-25 21:07:03 +0100

    Add Hr component and enhance Home layout with welcome message

---
+++ frontend/src/components/hr.tsx
@@ -0,0 +1,5 @@
+const Hr:React.FC = () => {
+    return <hr className="w-full mt-4 border border-shadow" />
+}
+
+export default Hr
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,14 +1,20 @@
 import NavButton from "../components/navbutton";
+import Hr from "../components/hr";
 
 const Home: React.FC = () => {
   return (
-    <div className="min-h-screen bg-primary text-white p-8">
+    <div className="min-h-screen bg-primary text-white p-4 md:p-8">
       <div className="nav flex items-center justify-between">
         <p className="grid text-left">
           <span>Sunday, </span>
           <span>15th October 2025</span>
         </p>
         <NavButton classNames="" text="Menu" />
+      </div>
+      <Hr />
+      <div className="body mt-10">
+        <h2 className="text-2xl font-poppins-bold">Hi, Peter</h2>
+        <p className="text-sm text-secondary">Welcome back, hope your doing well today?</p>
       </div>
     </div>
   );
