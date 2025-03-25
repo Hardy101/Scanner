78168fc73cdc63fc3da6c22614562b9e41571705
Eghosa Ordia <eghordia130@gmail.com>
2025-03-25 23:58:10 +0100

    Add GSAP dependency and implement dropdown state management in DropdownNav component

--- frontend/src/components/dropdownnav.tsx
+++ frontend/src/components/dropdownnav.tsx
@@ -1,8 +1,15 @@
 import Hr from "./hr";
+
+import { useDropdownState } from "../store/useDropdownStore";
+
 const DropdownNav: React.FC = () => {
+  const { setIsDropdownActive } = useDropdownState();
   return (
     <div className="w-5/6 absolute top-15 left-10 bg-[#225050] rounded-xl px-4 pt-10 pb-2 z-2">
-      <button className="absolute flex bg-shadow text-secondary rounded-md p-1 text-xl right-4 top-4">
+      <button
+        onClick={() => setIsDropdownActive(false)}
+        className="absolute flex bg-shadow text-secondary rounded-md p-1 text-xl right-4 top-4"
+      >
         <i className="lni lni-xmark"></i>
       </button>
       <ul className="text-sm text-secondary ">
