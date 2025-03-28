3ce07eabfd60ea46e5ee22660b63e26786f1cc05
Eghosa Ordia <eghordia130@gmail.com>
2025-03-28 18:28:18 +0100

    Add ActionButton component for reusable button functionality

---
+++ frontend/src/components/commonButton.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  classNames?: string;
+  text: string;
+}
+
+const ActionButton: React.FC<ButtonProps> = ({
+  classNames,
+  text,
+  ...props
+}) => {
+  return (
+    <button
+      {...props}
+      className={`${classNames} block box-shadow-1 font-poppins-bold px-4 py-2 rounded-md text-sm mt-8 ml-auto`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default ActionButton;
