4f2d5c485768ad2f0c7fe08c968a39cb1f85bec2
Eghosa Ordia <eghordia130@gmail.com>
2025-03-31 17:32:34 +0100

    Update CORS settings to restrict allowed origins for enhanced security

--- backend/invix/main.py
+++ backend/invix/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", 'http://localhost', 'http://localhost/', 'http://localhost:5173/'],  # ✅ Allow frontend URL
+    allow_origins=["http://localhost:5173/login", 'https://scanner-ohvw.onrender.com/auth/login'],  # ✅ Allow frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
