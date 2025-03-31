a9d542041bbb69b6f9930c8815be48938d73a165
Eghosa Ordia <eghordia130@gmail.com>
2025-03-31 09:05:55 +0100

    Update database URL for correct path and remove commented-out routes in main.py for cleaner code

--- backend/invix/main.py
+++ backend/invix/main.py
@@ -30,10 +30,6 @@ def check_role(required_role: str):
         return user
     return role_dependency
 
-# @app.get("/protected")
-# def protected_route(user: User = Depends(verify_token)):
-#     return {"message": f"Welcome, {user.email}! You have access."}
-
 @app.get("/admin-only")
 def admin_route(user: UserModel = Depends(check_role("admin"))):
     return {"message": "Welcome, Admin! This route is restricted."}
@@ -42,14 +38,6 @@ def admin_route(user: UserModel = Depends(check_role("admin"))):
 def event_manager_route(user: UserModel = Depends(check_role("event_manager"))):
     return {"message": "Welcome, Event Manager! You can manage events."}
 
-# @app.get("/users", response_model=list[PublicUser])
-# def get_all_users(db: Session = Depends(get_db), user: UserModel = Depends(verify_token)):
-#     # if user.role != "admin":
-#     #     raise HTTPException(status_code=403, detail="Insufficient permissions")
-    
-#     user_list = db.query(UserModel).all()
-#     return user_list
-
 @app.get("/users", response_model=List[PublicUser])
 def get_users(db: Session = Depends(get_db)):  # Injects the database session
     users = db.query(UserModel).all()
@@ -57,11 +45,3 @@ def get_users(db: Session = Depends(get_db)):  # Injects the database session
     return db.query(UserModel).all()
 
 # http://127.0.0.1:8000/
-
-@app.get("/test-db")
-def test_db(db: Session = Depends(get_db)):
-    try:
-        db.execute("SELECT 1")  # Simple query to test connection
-        return {"message": "Database is connected"}
-    except Exception as e:
-        return {"error": str(e)}
