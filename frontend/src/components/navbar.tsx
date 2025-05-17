47fcb85cc2fce587a2e5a05bed4a35f211d1aa26
Eghosa Ordia <eghordia130@gmail.com>
2025-05-17 13:18:13 +0100

    Refactor Navbar component styles for improved consistency and visual appeal. Update button and list styles to enhance user experience.

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -3,11 +3,11 @@ import { Link } from "react-router";
 const Navbar: React.FC = () => {
   return (
     <nav className="fixed w-full left-0 bottom-0 p-4 flex justify-center md:hidden z-5">
-      <ul className="w-full bg-black rounded-2xl py-4 px-4 text-white flex items-center justify-between md:w-1/4">
+      <ul className="w-4/5 bg-black rounded-full py-4 px-4 text-white flex items-center justify-between">
         <li>
           <Link
             to={"/"}
-            className="flex gap-4 items-center bg-primary px-4 py-2 rounded-2xl"
+            className="flex gap-4 items-center bg-primary px-4 py-2 rounded-full"
           >
             <i className="fa-solid fa-house text-2xl"></i>
             <span className="text-lg">Home</span>
