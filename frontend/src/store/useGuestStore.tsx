72c2ed6385573ba1fe7e51f83745e23acdd325b0
Eghosa Ordia <eghordia130@gmail.com>
2025-05-18 16:20:30 +0100

    Refactor useGuestStore to simplify state management by removing formData and related functions. Streamline guest management functionality for improved maintainability.

--- frontend/src/store/useGuestStore.tsx
+++ frontend/src/store/useGuestStore.tsx
@@ -1,13 +1,9 @@
 import { create } from "zustand";
-import { Guest, EventFormData } from "../constants/interfaces";
+import { Guest } from "../constants/interfaces";
 
 interface GuestStore {
   guest: Guest;
-  formData: EventFormData;
   setGuest: (guest: Guest) => void;
-  setFormData: (formData: EventFormData) => void;
-  resetGuest: () => void;
-  resetFormData: () => void;
 }
 
 const initialGuest: Guest = {
@@ -17,18 +13,7 @@ const initialGuest: Guest = {
   errors: "",
 };
 
-const initialFormData: EventFormData = {
-  name: "",
-  date: "",
-  location: "",
-  expected_guests: 0,
-};
-
 export const useGuestStore = create<GuestStore>((set) => ({
   guest: initialGuest,
-  formData: initialFormData,
   setGuest: (guest) => set({ guest }),
-  setFormData: (formData) => set({ formData }),
-  resetGuest: () => set({ guest: initialGuest }),
-  resetFormData: () => set({ formData: initialFormData }),
 }));
