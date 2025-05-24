6817368cc46b0ce5b7f839bff47afee8cb1e689d
Eghosa Ordia <eghordia130@gmail.com>
2025-05-24 18:17:24 +0100

    Update NavMenu component to include bottom margin for improved layout consistency.

--- frontend/src/components/eventDetails/navMenu.tsx
+++ frontend/src/components/eventDetails/navMenu.tsx
@@ -4,7 +4,7 @@ const NavMenu: React.FC = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="nav flex gap-8 items-center text-sm">
+    <div className="nav flex gap-8 items-center text-sm mb-4">
       <button
         onClick={() => navigate(-1)}
         className="bs-2 rounded-md bg-white px-2 py-1 hover:bg-primary hover:text-white text-base"
